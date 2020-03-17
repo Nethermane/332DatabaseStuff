886e637e67af872ad6bc91e912e46b70bb392132
Bruce Nishimura <16ban1@caslab.queensu.ca>
2020-03-16 20:23:28 -0400

    made it caps

--- header.php
+++ header.php
@@ -12,7 +12,7 @@
     <div class="collapse navbar-collapse" id="navbarSupportedContent">
       <ul class="navbar-nav mr-auto">
         <li class="nav-item active">
-          <a class="nav-link" href="Home.php">Home <span class="sr-only">(current)</span></a>
+          <a class="nav-link" href="home.php">Home <span class="sr-only">(current)</span></a>
         </li>
         <li class="nav-item">
           <a class="nav-link" href="AnimalSearch.php">Animal Search</a>
