78b4b1a3272fd5544ba4a856f701e3ad40a3881d
Katie Warburton <katie@Katherines-MacBook-7.local>
2020-04-04 18:14:57 -0400

    edits to home to make it prettier

--- home.php
+++ home.php
@@ -1,8 +1,19 @@
 <!DOCTYPE html>
 <html>
-<body>
-<?php
-require_once('header.php');
-?>
-</body>
+    <link href="someStyling.css" type="text/css" rel="stylesheet" >
+    <head>
+        <?php
+            require_once('header.php');
+        ?>
+    </head>
+    <body>
+    <br><br>
+        <h1 align = 'center'> <font color = 'green'> ANIMAL RESCUE NETWORK </font> </h1>
+        <h6 align = 'center'>  A website dedicated to providing you with information about rescue animals in Canada. </h6>
+        <br>
+        <div class="image" align = 'center'>
+        <img src = 'tommy.jpg' alt = 'tabby cat on chair' style="width:300px;height:300px;" >
+        <img src = 'mimi.jpg' alt = 'little dog' style="width:400px;height:300px;">
+        <img src = 'rosie.jpg' alt = 'grey kitten' style="width:300px;height:300px;"> </div>
+    </body>
 </html>
