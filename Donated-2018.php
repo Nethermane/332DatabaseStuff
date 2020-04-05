0721230db204942a50c0b7a863697c404311231b
Haadia <hmufti.24@gamil.com>
2020-04-05 10:43:37 -0400

    Donated-2018

--- Donated-2018.php
+++ Donated-2018.php
@@ -26,13 +26,13 @@ try {
 $sql = "";
 $count = "";
 if(!array_key_exists("searchValue",$_GET) || $_GET["searchValue"] == "") {
-    $sql = "SELECT * FROM `donation` INNER JOIN `organization` on donation.donate_to= organization.management_id  INNER JOIN `animal_management` on organization.management_id = animal_management.management_id where donation_date >= '2018-01-01'  AND donation_date <= '2018-12-31' 
+    $sql = "SELECT name,  SUM(amount) as TotalAmount FROM `donation`  JOIN `organization` on donation.donate_to= organization.management_id  INNER JOIN `animal_management` on organization.management_id = animal_management.management_id where donation_date >= '2018-01-01'  AND donation_date <= '2018-12-31' 
     GROUP BY organization.management_id " ;
     $count = "SELECT COUNT(*) FROM `donation` INNER JOIN `organization` on donation.donate_to= organization.management_id  INNER JOIN `animal_management` on organization.management_id = animal_management.management_id 
         GROUP BY organization.management_id " ;
 }
 else {
-    $sql = "SELECT * FROM donation INNER JOIN Organization on donation.donate_to= Organization.management_id  INNER JOIN animal_management on Organization.management_id= animal_management.management_id where name = '$_GET[searchValue]' AND donation_date > '2017-12-31' 
+    $sql = "SELECT name, SUM(amount) as TotalAmount FROM donation INNER JOIN Organization on donation.donate_to= Organization.management_id  INNER JOIN animal_management on Organization.management_id= animal_management.management_id where name = '$_GET[searchValue]' AND donation_date > '2017-12-31' 
          AND donation_date < '2019-01-01' 
          GROUP BY management_id " ;
     $count = "SELECT COUNT(*) FROM donation INNER JOIN Organization on donation.donate_to= Organization.management_id  INNER JOIN animal_management on Organization.management_id= animal_management.management_id " ;
@@ -48,7 +48,7 @@ if($res = $connec->query($count)){
         foreach ($connec->query($sql) as $row) {
             echo "<tr>";
                 echo "<td>" . $row['name'] . "</td>";
-                echo "<td>" . $row['amount'] . "</td>";
+                echo "<td>" . $row['TotalAmount'] . "</td>";
             echo "</tr>";
         }
         echo "</table></div>";
