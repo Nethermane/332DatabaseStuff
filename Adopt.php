fdf8e80b94d44643256130c6f457aab40d9d63a3
Bruce Nishimura <16ban1@caslab.queensu.ca>
2020-04-06 17:50:51 -0400

    Made adopt form use dropdowns

--- Adopt.php
+++ Adopt.php
@@ -1,20 +1,8 @@
 <?php
 require_once('header.php');
-echo '<div class="md-form mt-2 mb-2 ml-2 mr-2">
-<form method="get" action="Adopt.php">
-    <label for="animal_id">Animal ID</label>
-    <input type="text" id="animal_id" class="form-control mb-2" name="animal_id" class="searchInput" value=""/>
-    <label for="lastName">Last name</label>
-    <input type="text" id="lastName" class="form-control mb-2" name="last_name" class="searchInput" value="Donaldson"/>
-    <label for="phoneNum">Phone number</label>
-    <input type="text" id="phoneNum" class="form-control mb-2" name="phone_number" class="searchInput" value="5196197713"/>
-    <label for="paid">Paid($)</label>
-    <input type="text" id="paid" class="form-control mb-2" name="paid" class="searchInput" value="100"/>
-    <input type="submit" id="btnSubmit" name="btnSubmit" value="Adopt" />
-</form>
-</div>';
 ?>
 <?php
+$refill_fields = False;
 if(isset($_GET['btnSubmit'])) {
     try {
         $dbhost = 'localhost';
@@ -26,19 +14,81 @@ if(isset($_GET['btnSubmit'])) {
         echo "Error : " . $e->getMessage() . "<br/>";
         die();
     }
-    if(isset($_GET['animal_id']) && isset($_GET['last_name']) && isset($_GET['phone_number']) && isset($_GET['paid'])) {
-        $sql = "INSERT INTO adoption(animal_id,amount,last_name,phone_number) VALUES('" . $_GET['animal_id'] . "','" . $_GET['paid'] . "','" . $_GET['last_name'] . "','" . $_GET['phone_number'] . "');";
-        try {
-            $connec->prepare($sql)->execute();
-        } catch (PDOException $e) {
-            echo"<p>Invalid parameter " . $e . "</p>";
+
+    if(isset($_GET['animal_id']) && isset($_GET['last_name']) && isset($_GET['phone_number'])) {
+        $id = $_GET['animal_id'];
+        $last_name = $_GET['last_name'];
+        $phone_number = $_GET['phone_number'];
+        if(empty($id) || empty($last_name) || empty($phone_number)) {
+            echo"<p style='color: red'>Missing parameter</p>";
+        } else {
+            $sql = "";
+            if(isset($_GET['paid']) && !empty($_GET['paid'])) {
+                $sql = "INSERT INTO adoption(animal_id,amount,last_name,phone_number) VALUES('" . $_GET['animal_id'] . "','" . $_GET['paid'] . "','" . $_GET['last_name'] . "','" . $_GET['phone_number'] . "');";
+            } else {
+                $sql = "INSERT INTO adoption(animal_id,amount,last_name,phone_number) VALUES('" . $_GET['animal_id'] . "','0','" . $_GET['last_name'] . "','" . $_GET['phone_number'] . "');";
+            }
+            try {
+                if(!$connec->prepare($sql)->execute()) {
+                    echo"<p style='color: red'>Invalid Animal ID or family not in system</p>";
+                    $refill_fields = True;
+                } else {
+                    echo"<p style='color: green'>Sucessfully adopted animal no." . $id . "</p>";
+                }
+            } catch (PDOException $e) {
+                echo"<p style='color: red'>Invalid parameter " . $e . "</p>";
+            }
         }
     } else {
-        echo"<p>Missing parameter</p>";
+        echo"<p style='color: red'>Missing parameter</p>";
     }
     
     $connec = null;
 }
+?>
+<?php
+    try {
+        $dbhost = 'localhost';
+        $dbname='animal_rescue_application';
+        $dbuser = 'root';
+        $dbpass = '';
+        $connec = new PDO("mysql:host=$dbhost;dbname=$dbname", $dbuser, $dbpass);
+    } catch (PDOException $e) {
+        echo "Error : " . $e->getMessage() . "<br/>";
+        die();
+    }
+    $sql = "SELECT animal_id FROM animal where not exists (select animal_id from adoption where adoption.animal_id = animal.animal_id)";
+    $family = "SELECT last_name, phone_number FROM family";
+    echo '<div class="md-form mt-2 mb-2 ml-2 mr-2">
+    <form method="get" action="Adopt.php">
+        <label for="animal_id">Animal ID</label>
+        <select type="text" id="animal_id" class="form-control mb-2" name="animal_id" class="searchInput">
+        ';
+        foreach ($connec->query($sql) as $row) {
+            echo "<option value='". $row['animal_id'] . "'>" . $row['animal_id'] . "</option>";
+        }
+        echo '</select>
+        <label for="lastName">Last name</label>
+        <select type="text" id="lastName" class="form-control mb-2" name="last_name" class="searchInput">
+        ';
+        foreach ($connec->query($family) as $row) {
+            echo "<option value='". $row['last_name'] . "'>" . $row['last_name'] . "</option>";
+        }
+        echo '</select>
+        <label for="phoneNum">Phone number</label>
+        <select type="text" id="phoneNum" class="form-control mb-2" name="phone_number" class="searchInput"/>
+        ';
+        foreach ($connec->query($family) as $row) {
+            echo "<option value='". $row['phone_number'] . "'>" . $row['phone_number'] . " (". $row['last_name'] . ")</option>";
+        }
+        echo '</select>
+        <label for="paid">Paid($)</label>
+        <input type="text" id="paid" class="form-control mb-2" name="paid" class="searchInput" placeholder="100" value=""/>
+        <input type="submit" id="btnSubmit" name="btnSubmit" value="Adopt" />
+        </form>
+        </div>';
+?>
+<?php
 try {
     $dbhost = 'localhost';
     $dbname='animal_rescue_application';
